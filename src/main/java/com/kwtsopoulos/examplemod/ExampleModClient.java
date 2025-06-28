619442cb78e8c376da163c55ebe9036ac8b3ca97
JohnThyWizard <97440813+JohnThyWizard@users.noreply.github.com>
2025-06-28 18:05:39 +0300

    Initial commit

---
+++ src/main/java/com/kwtsopoulos/examplemod/ExampleModClient.java
@@ -0,0 +1,10 @@
+package com.kwtsopoulos.examplemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExampleModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
