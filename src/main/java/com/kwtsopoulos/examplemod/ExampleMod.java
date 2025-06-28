32d7b6fa7103474a61e77059af8e16b7b1f631b5
JohnThyWizard <97440813+JohnThyWizard@users.noreply.github.com>
2025-06-28 18:06:15 +0300

    added hmmm comment

--- src/main/java/com/kwtsopoulos/examplemod/ExampleMod.java
+++ src/main/java/com/kwtsopoulos/examplemod/ExampleMod.java
@@ -5,6 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// Hmmm comment
 public class ExampleMod implements ModInitializer {
 	public static final String MOD_ID = "examplemod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
